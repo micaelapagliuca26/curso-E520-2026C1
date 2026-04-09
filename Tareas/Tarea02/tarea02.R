d4c233009000f1bc58a022959abe774603542a2d
micaelapagliuca26 <52PA46002526@campus.economicas.uba.ar>
2026-04-08 23:42:49 -0300

    Tarea 02 terminada - Micaela

---
+++ Tareas/Tarea02/tarea02.R
@@ -0,0 +1,16 @@
+# TAREA 02 - Micaela
+mi_nombre <- "Micaela"
+materia <- "Ciencia de Datos"
+print(paste("Tarea realizada por:", mi_nombre))
+
+# Operaciones matemáticas (W3Schools)
+a <- 20
+b <- 5
+suma <- a + b
+multiplicacion <- a * b
+potencia <- a^2
+
+# Vectores y estadísticas
+mis_notas <- c(8, 9, 10, 7, 9)
+promedio_mica <- mean(mis_notas)
+print(paste("Mi promedio proyectado en", materia, "es:", promedio_mica))
