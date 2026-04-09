689b7e7b5bd4966c449ad43ef4f02d51914f5340
micaelapagliuca26 <52PA46002526@campus.economicas.uba.ar>
2026-04-09 13:04:03 -0300

    Tarea 02: Actualizada con nombre de alumna Micaela Pagliuca

---
+++ Tareas/Tarea02/tarea02rbase.R
@@ -0,0 +1,15 @@
+# Tarea 02 - R Base
+# Alumna: Micaela Pagliuca
+
+# Operaciones básicas
+a <- 10
+b <- 20
+suma <- a + b
+print(suma)
+
+# Ejemplo de vector y promedio
+notas <- c(8, 9, 7, 10, 6)
+promedio <- mean(notas)
+print(promedio)
+
+# Fin de la tarea
