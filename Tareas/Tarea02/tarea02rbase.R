67bc0aa4534deaade1e05049e5724c70ef2a0a65
micaelapagliuca26 <52PA46002526@campus.economicas.uba.ar>
2026-04-14 19:15:20 -0300

    Mejora Tarea 02 y archivos nuevos

--- Tareas/Tarea02/tarea02rbase.R
+++ Tareas/Tarea02/tarea02rbase.R
@@ -12,5 +12,67 @@ notas <- c(8, 9, 7, 10, 6)
 promedio <- mean(notas)
 print(promedio)
 
-# Fin de la tarea
+datos_estudiantes <- data.frame(
+  Nombre = c("Micaela", "Juan", "Lucas"),
+  Edad = c(22, 25, 21),
+  Nota = c(10, 8, 9)
+)
 
+dim(datos_estudiantes)
+summary(datos_estudiantes)
+
+desempeno <- factor(c("Alto", "Medio", "Alto", "Bajo"))
+# Ver los niveles
+levels(desempeno)
+
+# Gráfico de barras simple con las notas
+barplot(datos_estudiantes$Nota, 
+        names.arg = datos_estudiantes$Nombre, 
+        col = "blue", 
+        main = "Notas por Estudiante",
+        xlab = "Alumnos", 
+        ylab = "Calificación")
+
+# Crear una lista con diferentes tipos de objetos
+mi_resumen <- list(
+  titulo = "Informe de Clase",
+  cantidad_alumnos = nrow(estudiantes),
+  promedio_final = promedio
+)
+
+# Acceder a un elemento de la lista
+print(mi_resumen$titulo)
+# 1. Primero definimos el Data Frame
+estudiantes <- data.frame(
+  nombre = c("Mica", "Juan", "Poli", "Leo", "Santi"),
+  nota = c(10, 8, 9, 7, 6)
+)
+
+# 2.calculamos el promedio
+promedio <- mean(estudiantes$nota)
+
+# 3.crear la lista
+mi_resumen <- list(
+  titulo = "Informe de Clase",
+  cantidad_alumnos = nrow(estudiantes),
+  promedio_final = promedio
+)
+
+# 4. 
+print(mi_resumen)
+
+estudiantes <- data.frame(
+  nombre = c("Mica", "Juan", "Poli", "Leo", "Santi"),
+  nota = c(10, 8, 9, 7, 6)
+)
+
+promedio <- mean(estudiantes$nota)
+
+mi_resumen <- list(
+  titulo = "Informe de Clase",
+  cantidad_alumnos = nrow(estudiantes),
+  promedio_final = promedio
+)
+
+print(mi_resumen$titulo)
+print(mi_resumen)
